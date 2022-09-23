0d19d53d018fe78794768fafef7c9d3f355c5e73
Ashutosh Dubey <mail.ashutoshhq@gmail.com>
2022-09-23 12:36:24 +0530

    updated variable setup

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,7 +1,12 @@
-
 pub fn get_server_url() -> String {
-    if std::env::var("EGNITELY_ENV").expect("Unable to get database url") == "dev" {
-        return "https://dev.api.egnitely.com".to_string();
+    let egnitely_env = std::env::var("EGNITELY_ENV");
+    match egnitely_env {
+        Ok(egnitely_env) => {
+            if egnitely_env == "dev" {
+                return "https://dev.api.egnitely.com".to_string();
+            }
+            "https://api.egnitely.com".to_string()
+        }
+        Err(_err) => "https://api.egnitely.com".to_string(),
     }
-    "https://api.egnitely.com".to_string()
 }
